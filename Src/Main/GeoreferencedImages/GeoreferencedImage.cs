3dbf7a9510e4f04f7cb7f0f82b82917a3f935f0e
danielgoldbergtamu <daniel.goldberg@tamu.edu>
2020-02-17 16:24:08 -0600

    updated properties to use get; set; style

--- Src/Main/GeoreferencedImages/GeoreferencedImage.cs
+++ Src/Main/GeoreferencedImages/GeoreferencedImage.cs
@@ -10,35 +10,10 @@ namespace USC.GISResearchLab.Common.GeoreferencedImages
     {
         #region Properties
 
-        private string _SRS;
-        private double _X;
-        private double _XMax;
-        private double _Y;
-        private double _YMax;
-        private Image _Image;
-        private string _ImageFormat;
-        private int _Width;
-        private int _Height;
-        private double _Resolution;
 
-        public double Resolution
-        {
-            get { return _Resolution; }
-            set { _Resolution = value; }
-        }
-
-        public int Width
-        {
-            get { return _Width; }
-            set { _Width = value; }
-        }
-
-        public int Height
-        {
-            get { return _Height; }
-            set { _Height = value; }
-        }
-
+        public double Resolution { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
         public int Size
         {
             get
@@ -61,47 +36,13 @@ namespace USC.GISResearchLab.Common.GeoreferencedImages
             }
         }
 
-        public string ImageFormat
-        {
-            get { return _ImageFormat; }
-            set { _ImageFormat = value; }
-        }
-
-        public Image Image
-        {
-            get { return _Image; }
-            set { _Image = value; }
-        }
-
-        public string SRS
-        {
-            get { return _SRS; }
-            set { _SRS = value; }
-        }
-
-        public double X
-        {
-            get { return _X; }
-            set { _X = value; }
-        }
-
-        public double Y
-        {
-            get { return _Y; }
-            set { _Y = value; }
-        }
-
-        public double XMax
-        {
-            get { return _XMax; }
-            set { _XMax = value; }
-        }
-
-        public double YMax
-        {
-            get { return _YMax; }
-            set { _YMax = value; }
-        }
+        public string ImageFormat { get; set; }
+        public Image Image { get; set; }
+        public string SRS { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double XMax { get; set; }
+        public double YMax { get; set; }
 
         #endregion
     }
